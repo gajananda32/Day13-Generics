2a656057a21bc7bbae0ceddd881639ca5ad23c37
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-27 12:03:06 +0530

    Added double

---
+++ FlotMaximum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatMax_UC2
+{
+    internal class FlotMaximum
+    {
+        public static double  MaxFlote(double  firstValue,double  secondValue,double thirdValue)
+        {
+            //checking which value  is greater
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using FloatMax_UC2;
+using System;
+namespace FindMaxFlote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  output = FlotMaximum.MaxFlote(122.3,455.23,550.23);
+            Console.WriteLine("the maximum value is : "+output);
+        }
+    }
+}
