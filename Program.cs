6340172306576f2146babc9542bf3c62c08d7b69
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-27 12:04:41 +0530

    Generic String Maxumum

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using MaxString;
+using System;
+namespace StringUC3
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string output = StringMaxCheck.MaxString("55", "88", "99");
+            Console.WriteLine("The maximum string value is: "+output);
+        }
+    }
+}
