1d6aefa0c6c36de3d359672f9f5a7a8ed15ee0a0
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-27 12:01:57 +0530

    GenericIntegerMax

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using IntMaxUC1;
+using System;
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int output = IntegerAMax.MaximumIntegerNumber(55, 66, 77);
+            Console.WriteLine("Maximum Value is "+output);
+        }
+    }
+}
